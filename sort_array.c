31b8cbb24d54f6c1ec5967662025b4da043f009e
Romain Bouarah <Rom.Bou@hotmail.fr>
2020-03-14 21:59:37 +0100

    Add shuffle function for sort_array

--- sort_array.c
+++ sort_array.c
@@ -1,11 +1,12 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include "sort_array.h"
 
-/* create the default sort_array corresponding to identity with size s*/
-sort_array* sort_array_new (long s)
+/* create a sort_array corresponding to a permutation of size s*/
+sort_array* sort_array_new (long s, bool is_identity)
 {
   assert(s > 0);
   
@@ -19,11 +20,25 @@ sort_array* sort_array_new (long s)
   assert(arr->data);
 
   for(int i=0; i < s; i++)
-    arr->data[i]=i;
+    arr->data[i]=i+1;
+
+  if( !is_identity)
+    shuffle(arr);
   
   return arr;
 }
 
+
+void shuffle (sort_array *arr)
+{
+  srand(time(NULL)); // use current time as seed for random generator
+  for(int i=0; i < arr->size; i++)
+    {
+      int j = rand()%(arr->size - i) + i; 
+      if( i != j)
+	sort_array_swap(arr,i,j);
+    }
+}
 
 /* free a sort_array created by sort_array_new */
 void sort_array_free (sort_array *arr)
@@ -51,4 +66,15 @@ int sort_array_get (sort_array *arr, long i)
   assert(0 <= i && i < arr->size);
 
   return arr->data[i];
+}
+
+
+void sort_array_swap(sort_array *arr, long i, long j)
+{
+  assert(0 <= i && i < arr->size);
+  assert(0 <= j && j < arr->size);
+
+  int tmp = arr->data[i];
+  arr->data[i] = arr->data[j];
+  arr->data[j] = tmp;
 }
--- sort_array.h
+++ sort_array.h
@@ -1,6 +1,7 @@
 #ifndef SORT_ARRAY
 #define SORT_ARRAY
 
+#include <stdbool.h>
 
 typedef struct sort_array {
   long size;
@@ -8,7 +9,9 @@ typedef struct sort_array {
 } sort_array;
 
 
-sort_array* sort_array_new (long size);
+sort_array* sort_array_new (long size, bool is_identity );
+
+void shuffle (sort_array *array);
 
 void sort_array_free (sort_array *array);
 
@@ -16,5 +19,6 @@ void sort_array_set (sort_array *array, long index, int value);
 
 int sort_array_get (sort_array *array, long index);
 
+void sort_array_swap(sort_array *array, long i, long j);
 
 #endif
