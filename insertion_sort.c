c746b36fceecc95cf54c9e7c4c46465630237550
Romain Bouarah <Rom.Bou@hotmail.fr>
2020-03-22 23:12:21 +0100

    Change swap insertion mode to rotation insertion mode for insertion_sort

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,13 +1,19 @@
 #include "sorting_strategy.h"
 
-#define DELAY 1
+#define DELAY 5
 
 void insertion_sort(sort_array *arr)
 {
   const int n = sort_array_size(arr);
+  
   for(int i=1; i < n; i++) {
-    for(int j = i-1; j >= 0 && sort_array_get(arr, j) > sort_array_get(arr, j+1); j--) {
-      sort_array_swap(arr, j, j+1, DELAY);
+    int key = sort_array_get(arr, i);
+    int j = i - 1;
+    while( j >= 0 && sort_array_get(arr, j) > key ) {
+      sort_array_set(arr, j+1, sort_array_get(arr, j), DELAY);
+      j--;
     }
+    sort_array_set(arr, j+1, key, 1);    
   }
+  
 }
